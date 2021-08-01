40923b0f0eca4b1704b82e8c170600eadd623cb9
ANMOL12334 <61105838+ANMOL12334@users.noreply.github.com>
2021-08-01 12:52:02 +0530

    Update __telegraph.py

--- EvilBot/modules/__telegraph.py
+++ EvilBot/modules/__telegraph.py
@@ -6,7 +6,7 @@ import os
 from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
-Timesisnotwaiting = "ZAID"
+shrvan42 = "EVIL"
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=shrvan42)
 auth_url = r["auth_url"]
